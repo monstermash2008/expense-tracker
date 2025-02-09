df79287e219a3e5a99580001ef435467ebbc6360
Ryan <monstermash2008@gmail.com>
2025-02-09 18:18:59 +1300

    implement auth routes with Kinde

--- server/app.ts
+++ server/app.ts
@@ -2,13 +2,14 @@ import { Hono } from 'hono'
 import { logger } from 'hono/logger'
 import { expensesRoute } from './routes/expenses'
 import { serveStatic } from 'hono/bun'
+import { authRoute } from './routes/auth'
 
 const app = new Hono()
 
 // routes
 app.use("*", logger())
 
-const apiRoutes = app.basePath("/api").route("/expenses", expensesRoute)
+const apiRoutes = app.basePath("/api").route("/expenses", expensesRoute).route("/", authRoute)
 
 // serve static files
 app.get('*', serveStatic({ root: './frontend/dist' }))
